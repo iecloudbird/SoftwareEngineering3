4da6e8e4cb3ff1961513005ce96267a78b3c6c6b
EdwinVNM <edwin.vnm@gmail.com>
2024-10-09 16:23:36 +0100

    Update CommandLine.java
    test5

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.Scanner;
-
+//test5
 public class CommandLine {
 	
 	private static void listCustomerFuctionalityAvailable() {
