c33f540e62c99953f43e928899dcd851be6d900b
EdwinVNM <edwin.vnm@gmail.com>
2024-10-09 16:16:08 +0100

    test

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.Scanner;
-
+//test
 public class CommandLine {
 	
 	private static void listCustomerFuctionalityAvailable() {
