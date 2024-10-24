20377bdb9acd5e28d0c18213b18010c7f23772b2
EdwinVNM <edwin.vnm@gmail.com>
2024-10-24 20:03:09 +0100

    20:00 24/10/2024

---
+++ src/WarningLetter.java
@@ -0,0 +1,153 @@
+
+public class WarningLetter {
+	
+	private int id;
+	private String name;
+	private String address;
+	private String phoneNumber;
+	private String email;
+	private Boolean subscriptionStatus;
+
+
+	public Boolean getSubscriptionStatus() {
+		return subscriptionStatus;
+	}
+
+	public void setSubscriptionStatus(Boolean subscriptionStatus) {
+		this.subscriptionStatus = subscriptionStatus;
+	}
+
+	void setId(int custId) {
+		id = custId;
+	}
+	
+	void setName(String custName) {
+		name = custName;
+	}
+	
+	void setAddress(String custAddr) {
+		address = custAddr;
+	}
+	
+	void setPhoneNumber(String custPhone) {
+		phoneNumber = custPhone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	int getId() {
+		return id;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	String getAddress() {
+		return address;
+	}
+	
+	String getPhoneNumber() {
+		return phoneNumber;
+		
+	}
+	
+	public Customer() {
+		this.address =null;
+		this.name = null;
+		this.phoneNumber = null;
+		this.email = null;
+		this.subscriptionStatus = null;
+	}
+	
+	public Customer(String custName, String custAddr, String custPhone, String custEmail, Boolean custSubscriptionStatus) throws CustomerExceptionHandler  {
+		
+		id = 0;
+		
+		// Validate Input
+		try {
+			
+			validateName(custName);
+			validateAddress(custAddr);
+			validatePhoneNumber(custPhone);
+			validateEmail(custEmail);
+			
+		}
+		catch (CustomerExceptionHandler e) {
+			throw e;
+		}
+		
+		// Set Attributes
+		name = custName;
+		address = custAddr;
+		phoneNumber = custPhone;
+		email = custEmail;
+		subscriptionStatus = custSubscriptionStatus;
+	}
+	
+	public static void validateName(String custName) throws CustomerExceptionHandler {
+		
+		//Agree Formating Rules on "Customer Name"
+		//E.G. Name String must be a minimum of 2 characters and a maximum of 50 characters
+		
+		if (custName.isBlank() || custName.isEmpty())
+			throw new CustomerExceptionHandler("Customer Name NOT specified");
+		else if (custName.length() < 2)
+			throw new CustomerExceptionHandler("Customer Name does not meet minimum length requirements");
+		else if (custName.length() > 50)
+			throw new CustomerExceptionHandler("Customer Name exceeds maximum length requirements");
+		
+	}
+	
+	public static void validateAddress(String custAddr) throws CustomerExceptionHandler {
+		
+		//Agree Formating Rules on "Customer Address"
+		//E.G. Name String must be a minimum of 5 characters and a maximum of 60 characters
+		
+		if (custAddr.isBlank() || custAddr.isEmpty())
+			throw new CustomerExceptionHandler("Customer Address NOT specified");
+		else if (custAddr.length() < 5)
+			throw new CustomerExceptionHandler("Customer Address does not meet minimum length requirements");
+		else if (custAddr.length() > 60)
+			throw new CustomerExceptionHandler("Customer Address exceeds maximum length requirements");
+		
+	}
+	
+	public static void validatePhoneNumber(String custPhone) throws CustomerExceptionHandler {
+		
+		//Agree Formating Rules on "Customer PhoneNumber"
+		//E.G. Name String must be a minimum of 7 characters and a maximum of 15 characters
+		
+		if (custPhone.isBlank() || custPhone.isEmpty())
+			throw new CustomerExceptionHandler("Customer PhoneNumber NOT specified");
+		else if (!custPhone.matches("\\d+")) {  // Ensure it contains only digits
+	        throw new CustomerExceptionHandler("Customer PhoneNumber does not meet numeric format requirements");
+	    }
+		else if (custPhone.length() < 7)
+			throw new CustomerExceptionHandler("Customer PhoneNumber does not meet minimum length requirements");
+		else if (custPhone.length() > 15)
+			throw new CustomerExceptionHandler("Customer PhoneNumber exceeds maximum length requirements");
+		
+	}
+
+	public static void validateEmail(String email) throws CustomerExceptionHandler {
+			
+			//Agree Formating Rules on "Customer Email"
+			//E.G. Name String must be a minimum of 7 characters and a maximum of 15 characters
+			
+		if (email.isBlank() || email.isEmpty()) {
+	        throw new CustomerExceptionHandler("Customer Email NOT specified");
+	    } else if (email.length() < 5) {
+	        throw new CustomerExceptionHandler("Customer Email does not meet minimum length requirements");
+	    } else if (email.length() > 50) {
+	        throw new CustomerExceptionHandler("Customer Email exceeds maximum length requirements");
+	    }
+			
+		}
+}
---
+++ src/WarningLetterTest.java
@@ -0,0 +1,213 @@
+import junit.framework.TestCase;
+
+/*
+ * Customer Test Cases
+ * 1) Ensure Best Practices is applied to the codes (Meaning names, comments etc...),
+ * 2) 
+ */
+
+public class WarningLetterTest extends TestCase {
+	
+	//Test #: 1
+	//Test Objective: To create a Customer Account
+	//Inputs: custName = "Jack Daniels", custAddr = "Athlone", custPhone = "087-123123123", email = "jack@example.com"
+	// Expected Output: Customer Object created with id = 0, "Jack Daniels", "Athlone", "087-123123123", "jack@example.com"	//Expected Output: Customer Object created with id = 0, "Jack Daniels", custAddr = "Athlone", custPhone = "087-123123123"
+	public void testCreateCustomerSuccess() {
+		
+		//Create the Customer Object
+		try {
+			
+			// Call method under test
+            Customer custObj = new Customer("Jack Daniels", "Athlone", "0871231233", "jack@example.com",true);
+            
+            // Use getters to check for object creation
+            assertEquals(0, custObj.getId());
+            assertEquals("Jack Daniels", custObj.getName());
+            assertEquals("Athlone", custObj.getAddress());
+            assertEquals("0871231233", custObj.getPhoneNumber());
+            assertEquals("jack@example.com", custObj.getEmail());
+		}
+		catch (CustomerExceptionHandler e) {
+			fail("Exception not expected");
+		}
+		
+	}
+	
+	// Test #: 2
+    // Test Objective: To catch an invalid customer name
+    // Inputs: custName = "J"
+    // Expected Output: Exception Message: "Customer Name does not meet minimum length requirements"
+    
+    public void testInvalidCustomerNameLength() {
+        try {
+            // Call method under test
+            Customer.validateName("J");
+            fail("Exception expected");
+        } catch (CustomerExceptionHandler e) {
+            assertEquals("Customer Name does not meet minimum length requirements", e.getMessage());
+        }
+    }
+    
+    // Test #: 3
+    // Test Objective: To catch an invalid customer address
+    // Inputs: custAddr = "A"
+    // Expected Output: Exception Message: "Customer Address does not meet minimum length requirements"
+    
+    public void testInvalidCustomerAddressLength() {
+        try {
+            Customer.validateAddress("A");
+            fail("Exception expected");
+        } catch (CustomerExceptionHandler e) {
+            assertEquals("Customer Address does not meet minimum length requirements", e.getMessage());
+        }
+    }
+
+    // Test #: 4
+    // Test Objective: To successfully validate a customer address
+    // Inputs: custAddr = "Athlone"
+    // Expected Output: No exception, address is valid
+    
+    public void testValidCustomerAddress() {
+        try {
+            Customer custObj = new Customer("John Doe", "Athlone", "0874555757", "john@example.com",true);
+            assertEquals("Athlone", custObj.getAddress());
+        } catch (CustomerExceptionHandler e) {
+            fail("Exception not expected");
+        }
+    }
+    
+    // Test #: 5
+    // Test Objective: To catch an invalid customer phone number
+    // Inputs: custPhone = "8"
+    // Expected Output: Exception Message: "Customer PhoneNumber does not meet minimum length requirements"
+    
+    public void testInvalidCustomerPhoneNumberLength() {
+        try {
+            Customer.validatePhoneNumber("8");
+            fail("Exception expected");
+        } catch (CustomerExceptionHandler e) {
+            assertEquals("Customer PhoneNumber does not meet minimum length requirements", e.getMessage());
+        }
+    }
+
+    // Test #: 6
+    // Test Objective: To successfully validate a customer phone number
+    // Inputs: custPhone = "0874555757"
+    // Expected Output: No exception, phone number is valid
+    
+    public void testValidCustomerPhoneNumber() {
+        try {
+            Customer custObj = new Customer("John Doe", "Athlone", "0874555757", "john@example.com",true);
+            assertEquals("0874555757", custObj.getPhoneNumber());
+        } catch (CustomerExceptionHandler e) {
+            fail("Exception not expected");
+        }
+    }
+
+    // Test #: 7
+    // Test Objective: To catch an invalid customer email
+    // Inputs: email = "a@p"
+    // Expected Output: Exception Message: "Customer Email does not meet minimum length requirements"
+    
+    public void testInvalidCustomerEmailFormat() {
+        try {
+            Customer.validateEmail("a@p");
+            fail("Exception expected");
+        } catch (CustomerExceptionHandler e) {
+            assertEquals("Customer Email does not meet minimum length requirements", e.getMessage());
+        }
+    }
+
+    // Test #: 8
+    // Test Objective: To successfully validate a customer email
+    // Inputs: email = "jack@example.com"
+    // Expected Output: No exception, email is valid
+    
+    public void testValidCustomerEmail() {
+        try {
+            Customer custObj = new Customer("Jack Daniels", "Athlone", "0874555757", "jack@example.com",true);
+            assertEquals("jack@example.com", custObj.getEmail());
+        } catch (CustomerExceptionHandler e) {
+            fail("Exception not expected");
+        }
+    }
+    
+	 // Test #: 9
+	 // Test Objective: To validate the format of customer ID (e.g., #C00000)
+	 public void testValidateCustomerIDFormat() {
+		 try {
+		        // Arrange & Act
+		        Customer customer = new Customer("Jack Daniels", "Athlone", "0871234567", "jack@example.com",true);
+		        
+		        // Assert
+		        int expectedCustomerId = 0; 
+		        assertEquals(expectedCustomerId, customer.getId());
+		    } catch (CustomerExceptionHandler e) {
+		        fail("Exception was not expected.");
+		    }
+	 }
+	 
+	// Test #: 10
+	// Test Objective: Validate name of exactly 1 character
+	public void testCustomerNameMinBoundary() {
+	    try {
+	        Customer.validateName("AB");
+	        assertEquals("AB", "AB");
+	    } catch (CustomerExceptionHandler e) {
+	        fail("Exception not expected");
+	    }
+	}
+	
+	// Test #: 11
+	// Test Objective: Validate name of exactly 15 characters (boundary case)
+	public void testValidateNameMaxBoundary() {
+	    try {
+	    	String validName = "Alexanderson123";  // 15 characters
+	        Customer.validateName(validName);
+	        
+	      
+	        assertEquals(validName, "Alexanderson123");
+	    } catch (CustomerExceptionHandler e) {
+	        fail("Exception not expected");
+	    }
+	}
+	
+	// Test #: 12
+	// Test Objective: Validate address of exactly 5 character (boundary case)
+	public void testValidateAddressMinBoundary() {
+	    try {
+	        Customer.validateAddress("1glen");
+	        assertTrue(true);  
+	    } catch (CustomerExceptionHandler e) {
+	        fail("Exception not expected");
+	    }
+	}
+
+	// Test #: 13
+	// Test Objective: Validate address of exactly 100 characters (assuming max length is 100)
+	public void testValidateAddressMaxBoundary() {
+	    String longAddress = "A".repeat(60); //total 60 characters
+	    try {
+	        Customer.validateAddress(longAddress);
+	        assertTrue(true); 
+	    } catch (CustomerExceptionHandler e) {
+	        fail("Exception not expected");
+	    }
+	}
+	
+	// Test #: 14
+	// Test Objective: Catch invalid customer phone number containing special characters (dashes).
+    // Input: custPhone = "087-1234567"
+    // Expected Output: Exception with message "Customer PhoneNumber does not meet numeric format requirements".
+	public void testValidatePhoneNumberWithDashes() {
+	    try {
+	        Customer.validatePhoneNumber("087-1234567");
+	        fail("Exception expected"); 
+	    } catch (CustomerExceptionHandler e) {
+	        assertEquals("Customer PhoneNumber does not meet numeric format requirements", e.getMessage());
+	    }
+	}
+}
+
+		
+
