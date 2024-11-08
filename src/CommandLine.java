9e9e29063cdc7327fdd414cf1169e7f9556915f4
EdwinVNM <edwin.vnm@gmail.com>
2024-11-08 11:01:53 +0000

    ......

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -79,7 +79,19 @@ public class CommandLine {
 		System.out.println("99. Close the NewsAgent Application");
 		System.out.println("=============================================");
 		System.out.println(" ");
-		
+		///////////////////////////////////////////////////////////////////
+		System.out.println("101. Customer CRUD Actions");
+		System.out.println("102. Delivery Person CRUD Actions");
+		System.out.println("103. Invoice CRUD Actions");
+		System.out.println("104. Customer Publication Actions");
+		System.out.println("105. Order CRUD Actions");
+		System.out.println("106. Delivery Docket CRUD Actions");
+		System.out.println("107. Delivery Area CRUD Actions");
+		System.out.println("108. Warning Letter CRUD Actions");
+		System.out.println("109. Newsagent CRUD Actions");
+		System.out.println("110. Storage Record CRUD Actions");
+		System.out.println("=============================================");
+		System.out.println(" ");
 	}
 
 	
@@ -1490,6 +1502,9 @@ private static void updateDeliveryDocket(Scanner keyboard, MySQLAccess dao) {
 					    System.out.println("Closing the Application");
 					}
 					break;
+					
+				case "101":
+					customerCRUD();
 			
 				default:
 					System.out.println("No Valid Function Selected");
@@ -1509,6 +1524,148 @@ private static void updateDeliveryDocket(Scanner keyboard, MySQLAccess dao) {
 		
 
 	} // end main
+
+
+	private static void customerCRUD() {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello");
+		//Create the Database Object
+		
+				try {
+					
+					MySQLAccess dao = new MySQLAccess();
+
+					// Configure System for Running
+					Scanner keyboard = new Scanner(System.in); 
+					String functionNumber = "-99";
+					boolean keepAppOpen = true;
+				
+					while (keepAppOpen == true) {
+					
+						//Present list of functionality and get selection
+						//listCustomerFuctionalityAvailable();
+						listCustomerCRUDAvailable();
+						functionNumber = keyboard.next();
+						keyboard.nextLine(); 
+				
+						switch (functionNumber) {
+				
+						case "1":
+							//Get Customer Details from the User
+							System.out.printf("Enter Customer Name: \n");
+							String custName = keyboard.nextLine();
+							System.out.printf("Enter Customer Address: \n");
+							String custAddr = keyboard.nextLine();
+							System.out.printf("Enter Customer PhoneNumber: \n");
+							String custphoneNumber = keyboard.nextLine();
+							System.out.printf("Enter Customer Email: \n");
+							String custEmail = keyboard.nextLine();
+							System.out.println("Is the customer subscribed? (true/false): ");
+							while (!keyboard.hasNextBoolean()) {
+							    System.out.println("Invalid input. Please enter 'true' or 'false'.");
+							    keyboard.next(); // Consume invalid input
+							}
+							Boolean subscriptionStatus = keyboard.nextBoolean();
+							keyboard.nextLine();
+							
+							try {
+							    Customer custObj = new Customer(custName, custAddr, custphoneNumber, custEmail, subscriptionStatus);
+							  //Insert Customer Details into the database
+							    boolean insertResult = dao.insertCustomerDetailsAccount(custObj);
+							    if (insertResult) {
+							        System.out.println("Customer Details Saved");
+							    } else {
+							        System.out.println("ERROR: Customer Details NOT Saved");
+							    }
+							} catch (CustomerExceptionHandler e) {
+							    System.out.println("ERROR: " + e.getMessage());
+							}
+							
+						case "2": 
+							//Retrieve ALL Customer Records
+							try (ResultSet rSet = dao.retrieveAllCustomerAccounts()) {
+							    if (rSet != null) {
+							        boolean tablePrinted = printCustomerTable(rSet);
+							        if (tablePrinted) {
+							            rSet.close();
+							        }
+							    } else {
+							        System.out.println("No Records Found");
+							    }
+							} catch (SQLException e) {
+							    System.out.println("Error retrieving customer records: " + e.getMessage());
+							}
+							break;
+							
+						case "3":
+							updateCustomer(keyboard, dao);
+						    break;
+						
+						case "4":
+							//Delete Customer Record by ID
+							System.out.println("Enter Customer ID to be deleted or -99 to Clear all Rows:");
+							while (!keyboard.hasNextInt()) {
+							    System.out.println("Invalid input. Please enter a valid numeric Customer ID or -99.");
+							    keyboard.next();  // Consume invalid input
+							}
+							int deleteCustId = keyboard.nextInt();
+							if (deleteCustId == -99) {
+							    boolean deleteAllResult = dao.deleteAllCustomers();
+							    if (deleteAllResult) {
+							        System.out.println("Customer Table Emptied");
+							    } else {
+							        System.out.println("ERROR: Could not empty the table");
+							    }
+							} else {
+							    boolean deleteResult = dao.deleteCustomerById(deleteCustId);
+							    if (deleteResult) {
+							        System.out.println("Customer Deleted");
+							    } else {
+							        System.out.println("ERROR: Customer Details NOT Deleted or Do Not Exist");
+							    }
+							}
+							break;
+					        
+						case "99":
+							System.out.println("Are you sure you want to exit? (yes/no): ");
+							String exitConfirmation = keyboard.nextLine();
+							if (exitConfirmation.equalsIgnoreCase("yes")) {
+							    keepAppOpen = false;
+							    System.out.println("Closing the Application");
+							}
+							break;
+							
+						case "101":
+							customerCRUD();
+					
+						default:
+							System.out.println("No Valid Function Selected");
+							break;
+						} // end switch
+				
+					}// end while
+				
+					//Tidy up Resources
+					keyboard.close();
+				
+				}
+			
+				catch(Exception e) {
+					System.out.println("PROGRAM TERMINATED - ERROR MESSAGE:" + e.getMessage());
+				} // end try-catch
+	}
+
+
+	private static void listCustomerCRUDAvailable() {
+		// TODO Auto-generated method stub
+		System.out.println(" ");
+		System.out.println("=============================================");
+		System.out.println("Please choose ONE of the following options:");
+		System.out.println("1. Create Customer Account");
+		System.out.println("2. View ALL Customer Records");
+		System.out.println("3. Update Customer Record by ID");
+        System.out.println("4. Delete Inactive Customer Records");
+	}
 	
 	
 }
